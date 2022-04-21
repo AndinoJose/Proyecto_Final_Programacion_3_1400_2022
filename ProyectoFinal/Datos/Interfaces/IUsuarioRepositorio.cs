e62757408855504485ec71ef7f92089c91cb2fde
AndinoJose <jandino820@yahoo.com>
2022-04-21 10:06:23 -0600

    Commit 2
    Commit2

--- ProyectoFinal/Datos/Interfaces/IUsuarioRepositorio.cs
+++ ProyectoFinal/Datos/Interfaces/IUsuarioRepositorio.cs
@@ -18,6 +18,5 @@ namespace Datos.Interfaces
 
 
 
-
     }
 }
---
+++ ProyectoFinal/Datos/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using Datos.Interfaces;
+using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public Task<bool> Actualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Usuario>> Getlista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Usuario> GetPorCodigo(string Codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Nuevo(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
